10cebaaa83917701193c32e6a0bbc3550734b6d1
Muhammad Hamzah Saeed <102877887+Hamzah-Saeed@users.noreply.github.com>
2023-08-13 21:19:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
 
-
-
 // Sign up and Sign in Screen Change
 document.addEventListener("DOMContentLoaded", function () {
 
     let signUp = document.getElementById("signup_btn");
     let signIn = document.getElementById("signin_btn");
-    let userName = document.getElementById("name_field");
-    let title = document.getElementById("log_title");
 
-    signIn.onclick = function () {
-        userName.style.maxHeight = "0";
-        title.innerText = "Sign In";
-    }
+    document.getElementById("signin_btn").onclick = function () {
+        location.href = "signin.html";
+    };
 
-    signUp.onclick = function () {
-        userName.style.maxHeight = "65px";
-        title.innerText = "Sign Up";
-    }
+    document.getElementById("signup_btn").onclick = function () {
+        location.href = "signup.html";
+    };
 
 });
+
